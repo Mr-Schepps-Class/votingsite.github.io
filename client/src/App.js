4c29fc425cc653fda4d5b187ba213c00bbfcca55
asher.memon27 <asher.memon27@stgeorges.bc.ca>
2024-12-17 09:36:14 -0800

    Added header

--- client/src/App.js
+++ client/src/App.js
@@ -5,12 +5,23 @@ import './App.css';
 
 function App() {
     return (
+
+
+        <Home/>
+    );
+}
+function Home() {
+    return (
+
+
         <div className="App">
-            <h1>Test Title</h1>
+            <title>Web Portal - Home Page</title>
+            <Header/>
+            <h1>Web Portal</h1>
             <h1>File Upload Example</h1>
             <FileUpload />
             <RandomComponent text="wow cool using props amazing splendid spectacular stuff here man"/>
-            
+
         </div>
     );
 }
@@ -21,6 +32,21 @@ function BasicButton() {
       <button className="button-basic" >Basic Button Text</button>
     );
   }
+
+function Header() {
+    return (
+      <header className="header-default" >
+            <nav>
+                <ul>
+                    <li><a href="/">Web Portal</a></li>
+                    <li><a href="/">Gallery</a></li>
+                    <li><a href="/">Login/Signup</a></li>
+                    <li><a href="/">Upload Project</a></li>
+                </ul>
+            </nav>
+      </header>
+    );
+}
 
   function RandomComponent(props) {
     return (
