1c75e14c011717e41b8ec2db92ee3fa0dc04a739
asher.memon27 <asher.memon27@stgeorges.bc.ca>
2025-02-11 08:59:28 -0800

    thingy

---
+++ client/src/components/RatingScale.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const RatingScale = () => {
+  return (
+    <>
+      <label htmlFor="votingRange" className="form-label"></label>
+      <input
+        type="range"
+        className="form-range"
+        min="0"
+        max="10"
+        step="0.1"
+        id="votingRange"
+      />
+    </>
+  );
+};
+
+export default RatingScale;
---
+++ client/src/pages/DisplaySite.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import RatingScale from "../components/RatingScale";
+
+const DisplaySite = () => {
+  return (
+    <>
+      <RatingScale />
+    </>
+  );
+};
+
+export default DisplaySite;
