c02c146a281ea53815413fb3b013c467483827fe
ND0322 <nathandai03222@gmail.com>
2025-02-13 15:01:26 -0800

    fixed display

--- client/src/components/CardGrid.tsx
+++ client/src/components/CardGrid.tsx
@@ -2,11 +2,31 @@ import React from "react";
 import SiteCard from "./SiteCard";
 import DetectMobile from "./DetectMobile";
 import GridColumn from "./GridColumn";
+import { useState } from "react";
+import httpClient from "../httpClient";
 
 const CardGrid = () => {
+
+  const [size, setSize] = useState(0);
+
+  const getSize = async () => {
+    try {
+      const response = await httpClient.get("http://127.0.0.1:5000/getSize");
+
+      setSize(response.data.size);
+    } catch (error) {
+      console.error("Error getting data: ", error);
+    }
+  };
+
+  getSize();
+
+  console.log(size);
+
+
   return (
     <div className="container text-center">
-      <GridColumn totalSites={2} />
+      <GridColumn totalSites={size} />
     </div>
   );
 };
